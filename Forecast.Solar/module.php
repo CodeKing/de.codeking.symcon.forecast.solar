98821253d98e5a90063f4a43e32a56b790e55270
CodeKing <xH2n85L4m$ing>
2019-07-02 01:15:45 +0200

    timeout increased

--- Forecast.Solar/module.php
+++ Forecast.Solar/module.php
@@ -235,7 +235,7 @@ class ForecastSolar extends Module
 
         // curl options
         $curlOptions = [
-            CURLOPT_TIMEOUT => 10,
+            CURLOPT_TIMEOUT => 60,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_SSL_VERIFYHOST => false,
             CURLOPT_SSL_VERIFYPEER => false,
